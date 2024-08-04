3e4ccee4b27aecdf05ed840a4b531750a0ab094e
alijourney <alijourney116@gmail.com>
2024-08-03 23:15:20 -0400

    fix|fixed image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/portfolio",
-    output: 'export',
+  basePath: "/portfolio",
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 module.exports = nextConfig
